1e7bb86d411ea31f91f626c00b2c5b538a23764a
kenetharci <141351866+kenetharci@users.noreply.github.com>
2023-09-23 21:18:59 -0500

    Delete Diseño web directory

--- Diseño web/ContenidoJS/8.Modulos/metodosdearrays.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let cadMeses = "ENERO-FEBRERO-MARZO-ABRIL-MAYO-JUNIO";
-let myArray = cadMeses.split("-");
-console.log(myArray);
-
-//1. Buscar elementos en un arreglo sino lo encuntra devuevle undefuned
-const busqueda = myArray.find((e) => a === "FEBREROs");
-console.log(busqueda);
